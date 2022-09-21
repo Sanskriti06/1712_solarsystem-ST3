a801962213e4ca1946c69bfab1c27f7f8d7cc1c3
Sanskriti06 <sanskriti1712.be20@chitkara.edu.in>
2022-09-21 11:44:16 +0530

    added proj files

---
+++ script.js
@@ -0,0 +1,45 @@
+function calMercury(){
+  var w = document.getElementById("weight").value;
+  var mercury = w*0.38;
+  document.getElementById("merw").innerHTML = mercury;
+}
+function calVenus(){
+  var w = document.getElementById("weight").value;
+  var venus = w*0.91;
+  document.getElementById("venw").innerHTML = venus;
+}
+function calMars(){
+  var w = document.getElementById("weight").value;
+  var mercury = w*0.38;
+  document.getElementById("marw").innerHTML = mercury;
+}
+function calJupiter(){
+  var w = document.getElementById("weight").value;
+  var jupiter = w*2.34;
+  document.getElementById("jupw").innerHTML = jupiter;
+}
+function calSaturn(){
+  var w = document.getElementById("weight").value;
+  var saturn = w*1.06;
+  document.getElementById("satw").innerHTML = saturn;
+}
+function calUranus(){
+  var w = document.getElementById("weight").value;
+  var uranus = w*0.92;
+  document.getElementById("uraw").innerHTML = uranus;
+}
+function calNeptune(){
+  var w = document.getElementById("weight").value;
+  var neptune = w*1.19;
+  document.getElementById("nepw").innerHTML = neptune;
+}
+function calMoon(){
+  var w = document.getElementById("weight").value;
+  var moon = w*0.166;
+  document.getElementById("moow").innerHTML = moon;
+}
+function calEarth(){
+  var w = document.getElementById("weight").value;
+  var earth = w;
+  document.getElementById("earw").innerHTML = earth;
+}
